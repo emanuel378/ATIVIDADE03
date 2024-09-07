193a4f643167dd650d70a166878dcd3591e30bad
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-07 18:03:40 -0300

    Area de um retãngulo

---
+++ atv03.py
@@ -0,0 +1,5 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+lar = float(input("Digite a largura:"))
+alt = float(input("Digite a  altura:"))
+area = lar * alt
+print(f"A area do retañgulo e {area}")
